057bba1e6107524943f83b03795e7668d09e9525
Ammar Vora <ammarmust4@gmail.com>
2023-08-13 13:08:15 -0600

    Working on adding intro image on project page

--- portfolio/src/Components/ProjectPage.js
+++ portfolio/src/Components/ProjectPage.js
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
+
 import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm'
+
 import { useParams } from 'react-router-dom';
 
 import { getProject } from "../middleware/project-content";
 
 
 const ProjectPage = () => {
+  const [project, setProject] = useState("");
   const [content, setContent] = useState("");
   const { id: projectId } = useParams();
 
   useEffect(() => {
     const projectObj = getProject(projectId);
+    setProject(projectObj);
 
     fetch(projectObj.markdown)
     .then((res) => res.text())
@@ -22,12 +27,22 @@ const ProjectPage = () => {
 
 
     <div id="project-page">
+      <div className="project-intro-img" style={ {backgroundImage: `url(${project.img})`} }>
+      </div>
       <div className="project-intro">
-        <h1>This is the title of my project</h1>
+        <div>
+          <h1>{project.title}</h1>
+          <h3>{project.altText}</h3>
+        </div>
       </div>
 
+
       <div className="project-content">
-        <ReactMarkdown children={content} rehypePlugins={rehypeRaw}/>
+        <div className="project-page-gallery">
+          <h2>Gallery</h2>
+        </div>
+        <h2>Description</h2>
+        <ReactMarkdown children={content} rehypePlugins={[rehypeRaw, remarkGfm]} className="markdown"/>
       </div>
     </div>
 

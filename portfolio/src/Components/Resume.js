e1b315dde6610a08d82a0e5955ae1bae84c68a11
Ammar Vora <ammarmust4@gmail.com>
2023-08-16 14:50:19 -0600

    Resume section functionality done. Might need to add animations

--- portfolio/src/Components/Resume.js
+++ portfolio/src/Components/Resume.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import resume from '../resume.png';
 import { Worker } from '@react-pdf-viewer/core';
-import { Viewer, ScrollMode, SpecialZoomLevel } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
@@ -11,25 +11,31 @@ import resumeFile from '../resume.pdf';
 
 const Resume = () => {
 
-  const [pdfFile, setPDFFile] = useState(null);
-  const [viewPDF, setViewPDF] = useState(null);
-
+  const [viewPDF, setViewPDF] = useState(false);
 
   const defaultLayoutPluginInstance = defaultLayoutPlugin({
     sidebarTabs: () => {return [<></>]}, 
   });
 
+  useEffect(() => {
+    setViewPDF(false);
+  }, []);
+
   return ( 
     <div id='resume'>
-      <div id="resume-button">
-        <img src={resume} alt='Take a look at my Resume'></img>
-      </div>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-        <div id="pdf-container">
-          <div id="pdf-viewer">
-              <Viewer fileUrl={resumeFile} plugins={[defaultLayoutPluginInstance]}/>
-          </div>
+      {
+        <div id="resume-button" onClick={() => setViewPDF(!viewPDF) }>
+          <img src={resume} alt='Take a look at my Resume'></img>
         </div>
+      }
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        {viewPDF &&
+          <div id="pdf-container" onClick={() => setViewPDF(!viewPDF)}>
+            <div id="pdf-viewer" onClick={(event) => event.stopPropagation()}>
+                <Viewer fileUrl={resumeFile} plugins={[defaultLayoutPluginInstance]}/>
+            </div>
+          </div>
+        }
       </Worker>
 
     </div>
